d6dbcf9068c890fbaccec12b5b339e7991e3f6d2
Sam <samuelxborges@gmail.com>
2026-09-14 17:52:42 -0400

    add auth endpoint stubs with JSON contracts

---
+++ api/logout.php
@@ -0,0 +1,11 @@
+<?php
+// POST /api/logout.php
+//
+// send:     {}
+// returns:  { "error": "" }
+//
+// TODO: session_destroy().
+
+require_once __DIR__ . '/helpers.php';
+
+sendError('Not implemented');
