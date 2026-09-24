e4805f5a723da2652af3389811baae1434d063cd
NoahRossetti <noahdavidrossetti@gmail.com>
2026-09-23 21:23:33 -0400

    fixed search contacts

--- api/searchContacts.php
+++ api/searchContacts.php
@@ -12,20 +12,21 @@ $input = getJsonInput();
 $searchTerm = '%' . $input['search'] . '%';
 
 $searchQuery = $pdo->prepare("SELECT ContactID, FirstName, LastName, phone, email from Contacts WHERE UserID = ? AND CONCAT(FirstName,' ',LastName) LIKE ?");
-$searchQuery->execute($userID,$searchTerm);
+$searchQuery->execute([$userId,$searchTerm]);
 
 //checks if there is a match for the name searched
 if($searchQuery->rowCount() == 0)
 {
 	sendError('No Match');
 	
-}else{
-
+}
+else{
 //Creates array of associative arrays detailing information
-$searchResults = $searchQuery->fetchAll(PDO::FETCH_ASSOC);
-
-sendError("");
-return $searchResults;
+	
+	$searchResults = $searchQuery->fetchAll(PDO::FETCH_ASSOC);
+	return $searchResults;
+	sendError("");
+	
 }
 
 }
